5093ab0a03028e09ca18fe80b9d49c2ae4179a95
Julian S <200318@studierende.htl-donaustadt.at>
2023-11-17 01:57:24 +0100

    add vector store viewer (not implemented)

---
+++ app/data/page.tsx
@@ -0,0 +1,8 @@
+import { nanoid } from '@/lib/utils'
+import { Chat } from '@/components/chat'
+
+export const runtime = 'edge'
+
+export default function DataViewerPage() {
+  return <p className="text-2xl text-center">NOT IMPLEMENTED...</p>
+}
